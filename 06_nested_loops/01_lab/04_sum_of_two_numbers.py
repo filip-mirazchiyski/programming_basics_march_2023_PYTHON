c6b6000549daa9136120d1dda9c0d1bba52519f7
filip-mirazchiyski <124933131+filip-mirazchiyski@users.noreply.github.com>
2023-10-04 16:31:54 +0300

    Add files via upload

---
+++ 06_nested_loops/01_lab/04_sum_of_two_numbers.py
@@ -0,0 +1,27 @@
+starting_number = int(input())
+final_number = int(input())
+magic_number = int(input())
+
+combination_counter = 0
+magic_number_condition = False
+print_data = ""
+
+for x in range(starting_number, final_number + 1):
+    for y in range(starting_number, final_number + 1):
+
+        combination_counter += 1
+
+        if x + y == magic_number:
+            magic_number_condition = True
+            print_data = f"Combination N:{combination_counter} ({x} + {y} = {magic_number})"
+            break
+
+    if magic_number_condition:
+        break
+
+
+if magic_number_condition:
+    print(print_data)
+else:
+    print(f"{combination_counter} combinations - neither equals {magic_number}")
+
