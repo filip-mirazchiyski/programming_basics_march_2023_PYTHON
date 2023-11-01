9b1a70435667d7919eff42ed62997bb8de9ab4bf
filip-mirazchiyski <124933131+filip-mirazchiyski@users.noreply.github.com>
2023-11-01 11:15:05 +0200

    Delete 01_first_steps_in_coding/03_extra directory

--- 01_first_steps_in_coding/03_extra/circle_area_and_perimeter.py
+++
@@ -1,9 +0,0 @@
-from math import pi
-
-r = float(input())
-
-a = pi * (r ** 2)
-print("%.2f" % a)
-
-c = 2 * pi * r
-print("%.2f" % c)
--- 01_first_steps_in_coding/03_extra/trapezoid_area.py
+++
@@ -1,9 +0,0 @@
-b1 = float(input())
-b2 = float(input())
-h = float(input())
-
-area = (b1 + b2) * h / 2
-
-print("%.2f" % area)
-
-#https://www.freecodecamp.org/news/2f-in-python-what-does-it-mean/#:~:text=So%20%25.2f%20means%20to%20round%20up%20to%20two%20decimal%20places.
